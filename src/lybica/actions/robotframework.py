b59bbc78ce5a1c9058081738d7cf9ce20ba9f379
Zhang Yu <yu.3.zhang@nokia.com>
2015-11-05 13:40:13 +0800

    add output.xml combination and summary fetch support

--- src/lybica/actions/robotframework.py
+++ src/lybica/actions/robotframework.py
@@ -11,15 +11,15 @@ class RepeatRunningWrapper(object):
     CRITICAL = True
 
     def start_action(self, context):
-        # run the test and combine the output
+        # run the test
+        self.rpc = context.rpc
         context.RUN_TIMES = os.getenv('RUN_TIMES', 1)
         logging.info('run times: %d' % context.RUN_TIMES)
         self.output_dir = context.WORKSPACE
-        self.output_xml = os.path.join(self.output_dir, 'output.xml')
+        self.output_xml = 'output.xml'
         context.CASE_ROOT = os.getenv('CASE_ROOT', '.')
         logging.info('CASE_ROOT: %s' % context.CASE_ROOT)
-        output_files = self._run_test(context.RUN_TIMES, context.CASE_ROOT, self.output_dir)
-        self._combine_output(output_files, self.output_xml)
+        self._run_test(context.RUN_TIMES, context.CASE_ROOT, self.output_dir)
 
     def _run_test(self, run_times, case_root, output_dir):
         for _round in xrange(run_times):
@@ -28,20 +28,39 @@ class RepeatRunningWrapper(object):
                     'None', '--log', 'None', '-d', output_dir, '-o',\
                     'output-round-%d.xml' % (_round + 1), case_root])
 
-    def _combine_output(self, output_files, dst_file):
-        pass
+    def _combine_output(self, output_dir, dst_file):
+        # combine output files together
+        logging.info('combine output files together')
+        subprocess.call(['rebot', '-N', 'repeated', '-d', output_dir, '-o', dst_file, os.path.join(output_dir, 'output-round-*.xml')])
 
     def stop_action(self, context):
-        # parsing output.xml, generate log/report and post it to server
-        self._generate_log_and_report(self.output_xml)
-        summary = self._get_test_summary(self.output_xml)
+        # generate output.xml, log and report, post it to server
+        self._combine_output(self.output_dir, self.output_xml)
+        summary = self._get_test_summary(os.path.join(self.output_dir, self.output_xml))
         self._post_test_summary(summary)
 
-    def _generate_log_and_report(self, output_xml):
-        pass
-
     def _get_test_summary(self, output_xml):
-        pass
+        from robot.api import ExecutionResult
+        result = ExecutionResult(output_xml)
+        return {'passed': result.return_code == 0,
+                'total': {
+                    'total': result.statistics.total.all.total,
+                    'passed': result.statistics.total.all.passed,
+                    'failed': result.statistics.total.all.failed,
+                    'elapsed': result.statistics.total.all.elapsed,
+                },
+                'tests': self._get_summary_of_a_suite(result.suite)
+            }
+
+    def _get_summary_of_a_suite(self, suite):
+        result = []
+        for test in suite.tests:
+            result.append({'longname': test.longname,
+                'passed': test.passed,
+                'elapsed': test.elapsedtime})
+        for subsuite in suite.suites:
+            result.extend(self._get_summary_of_a_suite(subsuite))
+        return result
 
     def _post_test_summary(self, summary):
         pass
--- tst/test_lybica/actions/robotframework.py
+++ tst/test_lybica/actions/robotframework.py
@@ -11,6 +11,8 @@ class TestRepeatRunningWrapper(TestCase):
         self.context = Context()
         self.context.WORKSPACE = './ut_logs'
         self.context.CASE_ROOT = 'tst/testsuite'
+        if not os.path.exists(self.context.WORKSPACE):
+            os.makedirs(self.context.WORKSPACE)
         self.action = RepeatRunningWrapper()
 
     def tearDown(self):
@@ -20,4 +22,20 @@ class TestRepeatRunningWrapper(TestCase):
     def test_run_test_5_times(self):
         self.action._run_test(5, self.context.CASE_ROOT, self.context.WORKSPACE)
         self.assertEqual(len(os.listdir(self.context.WORKSPACE)), 5)
+
+    def test_combine_2_records_together(self):
+        from robot.api import ExecutionResult
+        shutil.copy('tst/testdata/output-round-1.xml', self.context.WORKSPACE)
+        shutil.copy('tst/testdata/output-round-2.xml', self.context.WORKSPACE)
+        self.action._combine_output(self.context.WORKSPACE, 'output.xml')
+        result = ExecutionResult(os.path.join(self.context.WORKSPACE, 'output.xml'))
+        self.assertEqual(result.statistics.total.all.total, 6)
+
+    def test_get_test_summary(self):
+        summary = self.action._get_test_summary('tst/testdata/output.xml')
+        self.assertEqual(summary['passed'], True)
+        self.assertEqual(summary['total']['total'], 6)
+        self.assertEqual(summary['total']['passed'], 6)
+        self.assertEqual(summary['total']['failed'], 0)
+        self.assertEqual(len(summary['tests']), 6)
 
