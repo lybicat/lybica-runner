845371f060bbc925fdac04671840b6417d7d66e0
Zhang Yu <yu.3.zhang@nokia.com>
2015-11-05 14:03:09 +0800

    fix rpc GET/POST issue

--- src/lybica/actions/__init__.py
+++ src/lybica/actions/__init__.py
@@ -1,1 +1,2 @@
 from .builtin import *
+from .robotframework import *
--- src/lybica/rpc.py
+++ src/lybica/rpc.py
@@ -17,39 +17,36 @@ class RPC(object):
         if not name.startswith('_'):
             if '__' in name:
                 name = name.replace('__', '/')
-            return _RestProxy(self.lybica_api_url, name, 'json')
-        raise AttributeError('Not found "%s" in AreaCI RPC.' % name)
+            return _JsonProxy(self.lybica_api_url, name)
+        raise AttributeError('Not found "%s" in RPC.' % name)
 
-class _RestProxy(object):
-    def __init__(self, base_url, name, context_type='json'):
+class _JsonProxy(object):
+    def __init__(self, base_url, name):
         self.base = base_url.strip('/')
-        self.context_type = context_type
         self.name = name
 
     def __call__(self, **kwds):
         '''Call remote interface by HTTP'''
         socket.setdefaulttimeout(30)
         api_url = '%s/%s' % (self.base, self.name)
-        if 'auto_check' in kwds:
-            logging.debug('auto_check set to: %s' % kwds['auto_check'])
-            self.auto_check = kwds['auto_check'] == 'true'
-
         logging.info('rpc:%s, param:%s' % (api_url, str(kwds)))
         response = self._get_response(api_url, kwds)
-
-        if not response:
-            raise RuntimeError('Failed to call remote interface, without any response.')
-
-        if self.context_type == 'json':
-            try:
-                return json.loads(response)
-            except ValueError:
-                logging.info('Error data:%s' % response)
-
-        return response
+        try:
+            return json.loads(response)
+        except ValueError:
+            logging.info('Error data:%s' % response)
+            raise
 
     @ignore_error(None)
     @retry(3, 30)
     def _get_response(self, api_url, kwds):
-        return urllib.urlopen(api_url, urllib.urlencode(kwds), proxies={}).read()
+        if kwds:
+            opener = urllib.urlopen(api_url, urllib.urlencode(kwds), proxies={})
+        else:
+            opener = urllib.urlopen(api_url, proxies={})
+
+        if opener.getcode() != 200:
+            raise RuntimeError('invalid return code: %d' % opener.getcode())
+
+        return opener.read()
 
