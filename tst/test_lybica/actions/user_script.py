cedab4c487db788ead5da788a9785ea38ca2e7fc
Zhang Yu <yu.3.zhang@nokia.com>
2015-11-12 15:58:00 +0800

    add more test

--- tst/test_lybica/actions/user_script.py
+++ tst/test_lybica/actions/user_script.py
@@ -17,7 +17,6 @@ class TestUserScriptAction(TestCase):
         if os.path.exists(self.context.WORKSPACE):
             shutil.rmtree(self.context.WORKSPACE)
 
-
     def test_run_do_ut_action_under_case_root(self):
         os.environ['CASE_ROOT'] = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'testsuite'))
         class _Action(_UserScriptAction):
@@ -33,3 +32,11 @@ class TestUserScriptAction(TestCase):
                 return 'do_lybica_ut'
 
         _Action()._run_script(self.context, {})
+
+    def test_run_do_undefined_action(self):
+        os.environ['CASE_ROOT'] = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'testsuite'))
+        class _Action(_UserScriptAction):
+            def get_script_name(self):
+                return 'do_undefined'
+
+        self.assertRaises(RuntimeError, _Action()._run_script, self.context, {})
